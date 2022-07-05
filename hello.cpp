4c72def138da687c11b3f6eb57980225530a2a83
yifeihong <108745488+yifeihong@users.noreply.github.com>
2022-07-05 23:31:39 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using std::cout;		//尽量不用using namespace std; 声明变量太多时
 using std::endl;		//可能会发生冲突
 int main(){
 	cout << "hello world!"<< endl;
-	cout << "hello "<< endl;
+	cout << "hello hi"<< endl;
 	cout << " 你好"<< endl;
  	system("pause");
 	return 0;
