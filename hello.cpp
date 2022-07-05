72caacc59cdd0cccf426a29aa74fe44f5ddfd1a0
yifeihong <108745488+yifeihong@users.noreply.github.com>
2022-07-05 23:29:17 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>	
+using std::cout;		//尽量不用using namespace std; 声明变量太多时
+using std::endl;		//可能会发生冲突
+int main(){
+	cout << "hello world!"<< endl;
+	cout << "hello "<< endl;
+	cout << " 你好"<< endl;
+ 	system("pause");
+	return 0;
+}  
